ec72bbc81b9279ec6163e9543d058cb81c13dd46
ido123ziv <idoshziv@gmail.com>
2022-07-09 14:10:20 +0000

    adding headers

--- assembler.h
+++ assembler.h
@@ -2,15 +2,49 @@
 order to provide central place for all of the constants and functions declarations */
 
 /* C libraries required for the project */
+/* ****************************************************
+    ****************************************************
+    ****************************************************
+    ************          Includes               *******
+    ****************************************************
+   ****************************************************
+*/
 #include <stdio.h>
 #include <string.h>
 #include <ctype.h>
 #include <stdlib.h>
 
+/* ****************************************************
+    ****************************************************
+    ****************************************************
+    ************          Enums                  *******
+    ****************************************************
+   ****************************************************
+*/
 typedef enum {FALSE,TRUE}boolean;
-enum filetypes {FILE_OBJECT,FILE_ENTRY,FILE_INPUT,FILE_EXTERN};
+typedef enum {FILE_OBJECT,FILE_ENTRY,FILE_INPUT,FILE_EXTERN}filetypes;
 
+/* ****************************************************
+    ****************************************************
+    ****************************************************
+    ************          Defines                *******
+    ****************************************************
+   ****************************************************
+*/
 #define MAX_EXTENSION_LENGTH 5
 #define ERROR 1
 
-
+/* ****************************************************
+    ****************************************************
+    ****************************************************
+    ************          Functions              *******
+    ****************************************************
+   ****************************************************
+*/
+/* ****** Global Functions ******* */
+int skip_whitespaces_at_begining(char c[], int start);
+int skip_whitespaces_at_end(char c[], int end);
+boolean isnumber(char number[]);
+char *create_file(char *original, int type);
+boolean syntax_validator(char operand[], int line_num, char *file_name);
+/* ****** Assembler.c ******* */
