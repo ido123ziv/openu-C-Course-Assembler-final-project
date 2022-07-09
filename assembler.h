c42142044397eba53d01dc6e93efbeba7fba85fa
codename470 <eran25670@gmail.com>
2022-07-09 12:49:59 +0000

    added make

---
+++ assembler.h
@@ -0,0 +1,9 @@
+/* This header file will be used for all the functions in the project in 
+order to provide central place for all of the constants and functions declarations */
+
+/* C libraries required for the project */
+#include <stdio.h>
+#include <string.h>
+#include <ctype.h>
+#include <stdlib.h>
+
---
+++ pre-assembler.c
@@ -0,0 +1,12 @@
+/*
+Project by Eran Cohen and Ido Ziv
+*/
+
+/*
+This script will replace the macros in the input to the original code that was include in
+the macro.
+It will run first before the first_transition happen
+*/
+
+#include "assembler.h"
+
