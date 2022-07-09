905c8e27248697c555ee868a3b21b3e21e7fba1e
ido123ziv <idoshziv@gmail.com>
2022-07-09 16:27:54 +0000

    adding constants

--- assembler.h
+++ assembler.h
@@ -23,6 +23,26 @@ order to provide central place for all of the constants and functions declaratio
 */
 typedef enum {FALSE,TRUE}boolean;
 typedef enum {FILE_OBJECT,FILE_ENTRY,FILE_INPUT,FILE_EXTERN}filetypes;
+/* ****************************************************
+    ****************************************************
+    ****************************************************
+    ************          Consts                *******
+    ****************************************************
+   ****************************************************
+*/
+const char *commands[] = {
+        "mov", "cmp", "add", "sub", "not", "clr", "lea", "inc", "dec", "jmp", "bne",
+        "get", "prn", "jsr", "rts", "hlt"
+};
+
+const char base32[32] = {
+        '!', '@', '#', '$', '%', '^', '&', '*', '<', '>', 'a', 'b', 'c',
+        'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p',
+        'q', 'r', 's', 't', 'u', 'v'};
+
+const char *directives[] = {
+        ".data", ".string", ".struct", ".entry", ".extern"
+};
 
 /* ****************************************************
     ****************************************************
@@ -38,6 +58,9 @@ typedef enum {FILE_OBJECT,FILE_ENTRY,FILE_INPUT,FILE_EXTERN}filetypes;
 /* Length Constants */
 #define LINE_LEN 82
 
+#define REGISTER_LENGTH 2 /* a register's name contains 2 characters */
+#define MIN_REGISTER 0 /* r0 is the first register */
+#define MAX_REGISTER 7 /* r7 is the last register */
 /* ****************************************************
     ****************************************************
     ****************************************************
@@ -48,7 +71,7 @@ typedef enum {FILE_OBJECT,FILE_ENTRY,FILE_INPUT,FILE_EXTERN}filetypes;
 /* ****** Global Functions ******* */
 int skip_whitespaces_at_begining(char c[], int start);
 int skip_whitespaces_at_end(char c[], int end);
-boolean isnumber(char number[]);
+boolean is_number(char number[]);
 char *create_file(char *original, int type);
 boolean syntax_validator(char operand[], int line_num, char *file_name);
 /* ****** Assembler.c ******* */
