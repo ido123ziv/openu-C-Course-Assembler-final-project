14d1e469c26a9a4cdde52705bae520490ad686c5
ido123ziv <idoshziv@gmail.com>
2022-07-19 11:15:44 +0000

    stashing add_label

--- phases.h
+++ phases.h
@@ -22,7 +22,7 @@ void check_operands(int cmd, boolean *is_src, boolean *is_dest);
 int read_line_am(char *line, int line_count);
 int check_for_label(char *line, boolean COLON);
 void phase_one(FILE *fp, char *file_name);
-labelPtr add_label(labelPtr *table, char *name, unsigned int address, int line_count);
+/*labelPtr add_label(labelPtr *table, char *name, unsigned int address, int line_count);*/
 boolean existing_label(labelPtr label, char *name);
 int handle_directive(int dir_type, char* line);
 int handle_data_directive(char * line);
