41c73f746f2f270c8a22f5106bea308ebff25e1b
ido123ziv <idoshziv@gmail.com>
2022-08-01 16:35:41 +0000

    auto testing

--- run_test.sh
+++ run_test.sh
@@ -1,10 +1,16 @@
 #!/bin/bash
-MY_PATH="/home/ubuntu/openu-C-Course-Assembler-final-project"
+MY_PATH="/home/ido/openu-C-Course-Assembler-final-project"
 if [[ $MY_PATH == $(pwd) ]]; then 
     echo good to go!; 
 else 
     cd $MY_PATH  
 fi
+rm Tests/*.am*
+rm Tests/*.ext*
+rm Tests/*.ent*
+mv Tests/expected.ob Tests/expected.save
+rm Tests/*.ob*
+mv Tests/expected.save Tests/expected.ob
 find ./Tests -type f -name "*.as" | while read line 
 do 
     FP=$(echo $line | cut -d '/' -f 3 | cut -d '.' -f 1)
